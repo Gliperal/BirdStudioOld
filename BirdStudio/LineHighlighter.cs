23173ce695b521dfc153d6176a6468d667981ba5
Gliperal <ananasviolet@fintmcwells.com>
2021-08-15 00:00:22 -0700

    Syntax highlighting.

---
+++ BirdStudio/LineHighlighter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Rendering;
+
+namespace BirdStudio
+{
+    public class LineHighlighter : IBackgroundRenderer
+    {
+        private TextEditor _editor;
+
+        public LineHighlighter(TextEditor editor)
+        {
+            _editor = editor;
+        }
+
+        public KnownLayer Layer
+        {
+            get
+            {
+                // draw behind selection
+                return KnownLayer.Selection;
+            }
+        }
+
+        public void Draw(TextView textView, DrawingContext drawingContext)
+        {
+            if (textView == null)
+                throw new ArgumentNullException("textView");
+            if (drawingContext == null)
+                throw new ArgumentNullException("drawingContext");
+            if (_editor.Document == null)
+                return;
+
+            textView.EnsureVisualLines();
+
+            // TODO if multiple lines selected?
+            var currentLine = _editor.Document.GetLineByOffset(_editor.CaretOffset);
+            foreach (var rect in BackgroundGeometryBuilder.GetRectsForSegment(textView, currentLine))
+            {
+                drawingContext.DrawRectangle(
+                    new SolidColorBrush(Color.FromArgb(0x40, 0, 0, 0xFF)), null,
+                    new Rect(rect.Location, new Size(textView.ActualWidth - 32, rect.Height)));
+            }
+
+            //VisualLine line = textView.VisualLines[9];
+            //Rect r = new Rect();
+            //r.X = 0;
+            //r.Y = line.VisualTop;
+            //r.Width = 100;
+            //r.Height = line.Height;
+            //drawingContext.DrawRectangle(Brushes.Red, null, r);
+        }
+    }
+}
--- BirdStudio/MainWindow.xaml.cs
+++ BirdStudio/MainWindow.xaml.cs
@@ -13,53 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-public class LineHighlighter : ICSharpCode.AvalonEdit.Rendering.IBackgroundRenderer
-{
-    private ICSharpCode.AvalonEdit.TextEditor _editor;
-
-    public LineHighlighter(ICSharpCode.AvalonEdit.TextEditor editor)
-    {
-        _editor = editor;
-    }
-
-    public ICSharpCode.AvalonEdit.Rendering.KnownLayer Layer
-    {
-        get
-        {
-            // draw behind selection
-            return ICSharpCode.AvalonEdit.Rendering.KnownLayer.Selection;
-        }
-    }
-
-    public void Draw(ICSharpCode.AvalonEdit.Rendering.TextView textView, DrawingContext drawingContext)
-    {
-        if (textView == null)
-            throw new ArgumentNullException("textView");
-        if (drawingContext == null)
-            throw new ArgumentNullException("drawingContext");
-        if (_editor.Document == null)
-            return;
-
-        textView.EnsureVisualLines();
-
-        // TODO if multiple lines selected?
-        var currentLine = _editor.Document.GetLineByOffset(_editor.CaretOffset);
-        foreach (var rect in ICSharpCode.AvalonEdit.Rendering.BackgroundGeometryBuilder.GetRectsForSegment(textView, currentLine))
-        {
-            drawingContext.DrawRectangle(
-                new SolidColorBrush(Color.FromArgb(0x40, 0, 0, 0xFF)), null,
-                new Rect(rect.Location, new Size(textView.ActualWidth - 32, rect.Height)));
-        }
-
-        //ICSharpCode.AvalonEdit.Rendering.VisualLine line = textView.VisualLines[9];
-        //Rect r = new Rect();
-        //r.X = 0;
-        //r.Y = line.VisualTop;
-        //r.Width = 100;
-        //r.Height = line.Height;
-        //drawingContext.DrawRectangle(Brushes.Red, null, r);
-    }
-}
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 
 namespace BirdStudio
 {
@@ -71,10 +29,19 @@ namespace BirdStudio
         public MainWindow()
         {
             InitializeComponent();
-        }
+            Assembly asm = Assembly.GetExecutingAssembly();
+            using (Stream s = asm.GetManifestResourceStream("BirdStudio.SyntaxHighlighting.xshd"))
+            {
+                if (s == null)
+                    throw new Exception();
+                using (XmlTextReader reader = new XmlTextReader(s))
+                {
+                    inputEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
 
-        private void ButtonTest_Click(object sender, RoutedEventArgs e)
-        {
+            inputEditor.Foreground = new SolidColorBrush(Color.FromRgb(180, 180, 180));
+            inputEditor.Background = new SolidColorBrush(Color.FromRgb(26, 26, 26));
             inputEditor.Text = @"#a
 console load 1
    1
@@ -98,7 +65,16 @@ console load 1
   15,U,X
    3,L,J,G
   40";
+        }
+
+        private void ButtonTest_Click(object sender, RoutedEventArgs e)
+        {
             inputEditor.TextArea.TextView.BackgroundRenderers.Add(new LineHighlighter(inputEditor));
+
+            var highlighting = inputEditor.SyntaxHighlighting;
+            var commentHighlighting = highlighting.NamedHighlightingColors.First(c => c.Name == "Comment");
+            commentHighlighting.Foreground = new SimpleHighlightingBrush(Color.FromRgb(0xC0, 0xC0, 0xC0));
+            inputEditor.SyntaxHighlighting = highlighting;
         }
     }
 }
